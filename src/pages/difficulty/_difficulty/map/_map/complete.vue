c3faf67761f02a973ea054a29408af81dd1797e8
yokoyama <you@example.com>
2018-12-31 18:15:03 +0900

    パズル終了画面：ボタン実装#18

---
+++ src/pages/difficulty/_difficulty/map/_map/complete.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p>パズル終了画面</p>
+    <button
+      class="button--green"
+      @click="retry"
+    >
+      もう一度チャレンジ
+    </button>
+    <button
+      class="button--green"
+      @click="otherMap"
+    >
+      別の画像で遊ぶ
+    </button>
+    <button
+      class="button--green"
+      @click="top"
+    >
+      終了ボタン
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    retry() {
+      const difficulty = this.$route.params.difficulty
+      const map = this.$route.params.map
+      this.$router.push('/difficulty/'+difficulty+'/map/'+map)
+    },
+    otherMap() {
+      const difficulty = this.$route.params.map
+      this.$router.push('/difficulty/'+difficulty+'/map')
+    },
+    top() {
+      this.$router.push('/')
+    }
+  }
+}
+
+
+
+</script>
--- src/pages/difficulty/_id/map/_map/complete.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>パズル終了画面</p>
-  </div>
-</template>
