27871e8033b63b601d139a3d01ab33586e2e1907
yokoyama <you@example.com>
2018-12-31 17:51:38 +0900

    スタート画面：ボタン実装#13

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
     <p>スタート画面</p>
+    <button
+      class="button--green"
+      @click="start"
+    >
+      ゲームスタート
+    </button>
+    <button
+      class="button--green"
+      @click="gallery"
+    >
+      画像ギャラリー
+    </button> 
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    start() {
+      this.$router.push('/difficulty')
+    },
+    gallery() {
+      this.$router.push('/gallery')
+    },
+  }
+}
+</script>
