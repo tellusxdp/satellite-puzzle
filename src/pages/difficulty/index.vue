50434227430bf7fa42411ad41069b2777acaf0b6
yokoyama <you@example.com>
2018-12-31 17:54:02 +0900

    難易度選択画面：ボタン実装#14

--- src/pages/difficulty/index.vue
+++ src/pages/difficulty/index.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
     <p>難易度選択画面</p>
+    <button
+      class="button--green"
+      @click="easy"
+    >
+      初級
+    </button>
+    <button
+      class="button--green"
+      @click="normal"
+    >
+      中級
+    </button>
+    <button
+      class="button--green"
+      @click="hard"
+    >
+      上級
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    easy() {
+      this.$router.push('/difficulty/easy/map')
+    },
+    normal() {
+      this.$router.push('/difficulty/normal/map')
+    },
+    hard() {
+      this.$router.push('/difficulty/hard/map')
+    },
+  }
+}
+</script>
