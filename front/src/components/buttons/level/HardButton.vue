b07a936331cda9275e16f4b171a4b95499e59e55
natsuki-y <you@example.com>
2019-02-11 13:52:43 +0900

    feat: add level-image

---
+++ front/src/components/buttons/level/HardButton.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  button.hard-button(@click="click")
+    img(src="~assets/images/level/level_hard.png")
+</template>
+
+<script>
+export default {
+  methods: {
+    click () {
+      this.$emit('onClick')
+    }
+  }
+}
+</script>
