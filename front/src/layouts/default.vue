2fe04cb757d37e6126a89d8f5618e36d6e7a7b93
natsuki-y <you@example.com>
2019-02-12 23:00:22 +0900

    fix: scroll pc

--- front/src/layouts/default.vue
+++ front/src/layouts/default.vue
@@ -7,6 +7,13 @@
 <script>
 export default {
   mounted () {
+    const deviceType = this.$ua.deviceType()
+    // PCの場合はスクロール調整を行わない
+    console.log(deviceType)
+    if (deviceType === 'pc') {
+      return
+    }
+
     var mapList = this.$el.getElementsByClassName('map-list')
 
     window.addEventListener('touchmove', function(event) {
@@ -30,7 +37,7 @@ export default {
 </script>
 
 
-<style>
+<style lang="scss" scoped>
 html {
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
@@ -41,7 +48,6 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
   overflow: hidden;
-  height: 100%;
 }
 
 .contents {
@@ -49,9 +55,6 @@ html {
   background-size: contain;
   width: 640px;
   height: 1148px;
-
-  /* height: 100vh;
-  width: calc(100vh / 1149 * 640); */
   margin-left: auto;
   margin-right: auto;
 }
@@ -59,7 +62,7 @@ html {
 .wrapper {
   background-color: #000;
   width: 100vw;
-  height: 100vh;
+  height: 1148px;
   z-index: -10;
 }
 
